bf02c26c5dc66fa7f744d3c229b972ca205b4def
Luciano J. Ferreira <ljferreira512@gmail.com>
2023-10-09 16:36:00 -0300

    desenvolvimnto do carrossel

--- ForeverHomesShelter/js/script.js
+++ ForeverHomesShelter/js/script.js
@@ -1,14 +1,39 @@
-//const showModalDonate = document.getElementById("botaoDonate");
-/*exibeModalDonate.addEventListener('click', () => {
-    alert("teste");
-});*/
+var bancoImagem = ["../img/png/bob.png",
+                   "../img/png/clorinde.png", 
+                   "../img/png/roney.png", 
+                   "../img/png/flocks.png", 
+                   "../img/png/aristotle.png"];
+
 
 exibeModalDonate = () => {
     const modal = document.getElementById("modalDonate");
     modal.style.display = "block";
+    modal.style.animationName = "exibe";
 }
 
 escondeModalDonate = () => {
     const modal = document.getElementById("modalDonate");
-    modal.style.display = "none";
+    modal.style.animationName = "oculta";
+}
+
+carrosselEsq = () => {
+
+    let imagem = bancoImagem.shift();
+    bancoImagem.push(imagem);
+
+    for(let conta = 0 ; conta < 4 ; conta++){
+        document.getElementById("img00" + (conta + 1)).src = bancoImagem[conta];
+    }
+
+}
+
+carrosselDir = () => {
+
+    let imagem = bancoImagem.pop();
+    bancoImagem.unshift(imagem);
+
+    for(let conta = 0 ; conta < 4 ; conta++){
+        document.getElementById("img00" + (conta + 1)).src = bancoImagem[conta];
+    }
+
 }
